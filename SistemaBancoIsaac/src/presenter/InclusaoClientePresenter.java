428001d5e9aef1ae6560b7b4e21fd036568ca6a2
isaacvieiramorais@gmail.com <isaacvieiramorais@gmail.com@9773fff7-97c9-f280-3ddc-9d9f9b262fdd>
2013-06-14 18:41:05 +0000

    Caso de uso Manter cliente feito

--- SistemaBancoIsaac/src/presenter/InclusaoClientePresenter.java
+++ SistemaBancoIsaac/src/presenter/InclusaoClientePresenter.java
@@ -11,6 +11,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.ParseException;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import model.cliente.Cliente;
 import model.cliente.PessoaFisica;
@@ -24,16 +26,16 @@ import view.InclusaoClienteView;
  * @author Isaac
  */
 public class InclusaoClientePresenter {
-
+    
     private ICollection clientes;
     private Cliente clienteAtual;
     private InclusaoClienteView view;
     private Funcionario funcionario;
-
+    
     public static void main(String args[]) {/////////////////////////teste
         new InclusaoClientePresenter(null, null, null).constroiInclusao();
     }
-
+    
     public InclusaoClientePresenter(ICollection clientes, Cliente clienteAtual, Funcionario fun) {
         this.clientes = clientes;
         this.funcionario = funcionario;
@@ -48,7 +50,7 @@ public class InclusaoClientePresenter {
         }
         view.setVisible(true);
     }
-
+    
     private void constroiEdicao() {
         if (funcionario instanceof Gerente) {
             if (clienteAtual.isBloqueado()) {
@@ -62,10 +64,30 @@ public class InclusaoClientePresenter {
             }
             view.getBotaoSalvar().setText("Editar");
             view.getCampoSenha().setEditable(false);
-            view.getCampoCpf().setText(clienteAtual.get);
+            view.getCampoCpf().setText(clienteAtual.getDocumento());
+            view.getCampoData().setText(new FormatadorData().convertToString(clienteAtual.getData()));
+            view.getCampoEndereco().setText(clienteAtual.getEndereco());
+            view.getCampoNome().setText(clienteAtual.getNome());
+            view.getCampoUser().setText(clienteAtual.getNomeUsuario());
+            view.getBotaoCancelar().addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    cancelar();
+                }
+            });
+            view.getBotaoSalvar().addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    try {
+                        editar();
+                    } catch (ParseException ex) {
+                        JOptionPane.showMessageDialog(view, ex.getMessage());
+                    }
+                }
+            });
         }
     }
-
+    
     private void constroiInclusao() {
         view.getBotaoDesbloquear().setEnabled(false);
         view.getGrupoButtonClientes().setSelected(view.getRadioFisica().getModel(), true);
@@ -97,24 +119,24 @@ public class InclusaoClientePresenter {
                 }
             }
         });
-
+        
     }
-
+    
     private void interfacePessoaFisica() {
         view.getLabelCpf().setText("CPF:");
         view.getLabelData().setText("Data de nascimento:");
     }
-
+    
     private void interfacePessoaJuridica() {
-
+        
         view.getLabelCpf().setText("CNPJ:");
         view.getLabelData().setText("Data de abertura:");
     }
-
+    
     private void cancelar() {
         view.dispose();
     }
-
+    
     private void salvar() throws ParseException {
         String nome = view.getCampoNome().getText();
         String endereco = view.getCampoEndereco().getText();
@@ -134,6 +156,27 @@ public class InclusaoClientePresenter {
             }
             clientes.incluir(clienteAtual);
             JOptionPane.showMessageDialog(view, "Cliente incluido com sucesso!");
+        }
+    }
+    
+    private void editar() throws ParseException {
+        String doc = view.getCampoCpf().getText();
+        String data = view.getCampoData().getText();
+        String end = view.getCampoEndereco().getText();
+        String nome = view.getCampoNome().getText();
+        String user = view.getCampoUser().getText();
+        if (doc.isEmpty() || data.isEmpty() || end.isEmpty()
+                || nome.isEmpty() || user.isEmpty()) {
+            JOptionPane.showMessageDialog(view, new FaltaPreencherCamposException());
+        } else {
+            clienteAtual.setData(new FormatadorData().convertToDate(data));
+            clienteAtual.setDocumento(doc);
+            clienteAtual.setEndereco(end);
+            clienteAtual.setNome(nome);
+            clienteAtual.setNomeUsuario(user);
+            clientes.editar(clienteAtual);
+            JOptionPane.showMessageDialog(view, "Cliente editado com sucesso!");
+            view.dispose();
         }
     }
 }
